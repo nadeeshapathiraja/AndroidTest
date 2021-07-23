d6425e8a151980ff096fd52b15efcc2ceffabbf4
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-23 23:31:58 +0530

    Commit all

--- Handelers/app/src/main/java/com/example/handelers/MainActivity.java
+++ Handelers/app/src/main/java/com/example/handelers/MainActivity.java
@@ -70,11 +70,11 @@ public class MainActivity extends AppCompatActivity {
                 for(int i=totalMiliSecond; i>=0; i--){
                     try {
                         Thread.sleep(1);
-                        h = i/3600000;
-                        m = (i%3600000)/60000;
-                        s = ((i%3600000)%60000)/1000;
-                        ms = ((i%3600000)%60000)%1000;
-                        if(h==0 && m==0 && s==0 && ms==0){
+                        // h = i/3600000;
+                        // m = (i%3600000)/60000;
+                        // s = ((i%3600000)%60000)/1000;
+                        // ms = ((i%3600000)%60000)%1000;
+                        // if(h==0 && m==0 && s==0 && ms==0){
                             //Add extra servicesg
                         }
                         handle.sendEmptyMessage(0);
